9e5a041db9b6ec98b963781fdf587c1651a94a1d
CollinsLiam21 <35492820+CollinsLiam21@users.noreply.github.com>
2018-01-29 08:32:31 -0500

    Updated from Brython Server: 1/29/2018 8:32:30 AM

--- movie.py
+++ movie.py
@@ -1,1 +1,16 @@
+#Liam Collins
+#1/29/18
+#movie.py
 
+age = int(input('Enter your age: '))
+
+if age >= 18:
+    print('You can watch R rated movies')
+elif age >= 13:
+    print('You can watch PG-13 movies')
+elif age >= 7:
+    print('You can watch PG movies')
+elif age >= 0:
+    print('You can watch G movies')
+else:
+    print('No movies for you!, your not even alive yet')
