d6f6860bf7e7fc249e7b3eb00e3f3f15407653bb
CollinsLiam21 <35492820+CollinsLiam21@users.noreply.github.com>
2018-01-29 08:26:13 -0500

    Updated from Brython Server: 1/29/2018 8:26:13 AM

--- gradeCalculator.py
+++ gradeCalculator.py
@@ -1,1 +1,17 @@
+#Liam Collins
+#1/29/18
+#gradeCalculator.py
+
+grade = round(float(input('Enter your percentage grade: ')))
+
+if grade>=90:
+    print('You earned an A')
+elif grade>=80:
+    print('You earned a B')
+elif grade>=70:
+    print('You earned a C')
+elif grade>=60:
+    print('You earned a D')
+else:
+    print('You earned an NC, try harder!')
 
