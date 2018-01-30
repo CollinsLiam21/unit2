579420f0f9fd6729580e2bae5697ce7a69157f5e
CollinsLiam21 <35492820+CollinsLiam21@users.noreply.github.com>
2018-01-30 08:18:56 -0500

    Updated from Brython Server: 1/30/2018 8:18:55 AM

--- compoundDemo.py
+++ compoundDemo.py
@@ -1,1 +1,16 @@
+#Liam Collins
+#1/30/18
+#compoundDemo.py - how to use and/or
 
+num = int(input('Enter a number: '))
+
+if num > 0 and num%7==0:
+    print(num, 'is positive and divisible by 7')
+elif num > 0:
+    print(num, 'is positive and not divisble by 7')
+elif num < 0 and num%7==0:
+    print(num, 'is negative and divisble by 7')
+elif num < 0:
+    print(num, 'is negative and not divisble by 7')
+else:
+    print(num, 'is zero')
