54db7ba5c8291b0ea721ecaabe6d2d9d3da12ae1
CollinsLiam21 <35492820+CollinsLiam21@users.noreply.github.com>
2018-01-30 08:49:44 -0500

    Updated from Brython Server: 1/30/2018 8:49:43 AM

--- adventure.py
+++ adventure.py
@@ -1,1 +1,16 @@
+#Liam Collins
+#1/30/18
+#adventure.py
 
+print('Earl is running at you with the ball')
+challenge = print('Do you challenge him?')
+
+if challenge = 'yes':
+    print('Ok you are in a bit of a pickle because Earl is much better at soccer than you')
+    run = print('You have one more chance to give up and run away, do you turn the other way and run?')
+    if run == 'yes':
+        print('Good job! Your ankles were not broken but unfortunately Earl scored on your team and you lost the game')
+    if run == 'no':
+        print('You should have run away, Earl broke your ankles and he scored on you. You lose!')
+if challenge = 'no':
+    print('You win!')
