4b458a1a8a5ad118842f6c13f25a8338269a1d6a
CollinsLiam21 <35492820+CollinsLiam21@users.noreply.github.com>
2018-01-30 08:25:54 -0500

    Updated from Brython Server: 1/30/2018 8:25:52 AM

--- fortuneTeller.py
+++ fortuneTeller.py
@@ -1,1 +1,25 @@
+#Liam Collins
+#1/30/18
+#fortuneTeller.py
 
+color = input('Pick red or blue: ')
+num = int(input('Pick number from 1-4: '))
+
+if color=='red' and num==1:
+    print('You will be megged by Christo')
+elif color=='red' and num==2:
+    print('You will be megged by Earl')
+elif color=='red' and num==3:
+    print('You will be megged by Liam')
+elif color=='red' and num==4:
+    print('You will be megged by Aiden')
+elif color=='blue' and num==1:
+    print('You will meg Dillon')
+elif color=='blue' and num==2:
+    print('You will meg Christo')
+elif color=='blue' and num==3:
+    print('You will meg Dillon twice')
+elif color=='blue' and num==4:
+    print('You will meg Christo three times')
+else:
+    print('ERROR, fix num or color')
