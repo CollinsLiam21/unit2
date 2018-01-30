b44c3bb4a2dd0078e3b5889ab9434c7d01896fbe
CollinsLiam21 <35492820+CollinsLiam21@users.noreply.github.com>
2018-01-30 08:31:30 -0500

    Updated from Brython Server: 1/30/2018 8:31:29 AM

--- insulter.py
+++ insulter.py
@@ -1,1 +1,20 @@
+#Liam Collins
+#1/30/18
+#insulter.py
 
+from random import randint
+
+name = input('Enter your name: ')
+
+num = randint(1,5)
+
+if num==1:
+    print('You are dumb')
+if num==2:
+    print('You need help')
+if num==3:
+    print('You are going to die from pure stupidity')
+if num==4:
+    print('My grandma could play better than you')
+if num==5:
+    print('Your not smarter than a fifth grader')
