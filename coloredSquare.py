cb90e4b343d3355eeeb14b3485987c53d6be2222
CollinsLiam21 <35492820+CollinsLiam21@users.noreply.github.com>
2018-01-31 17:28:49 -0500

    Updated from Brython Server: 1/31/2018 5:28:48 PM

--- coloredSquare.py
+++ coloredSquare.py
@@ -3,35 +3,47 @@
 #coloredSquare.py
 
 from ggame import*
+from random import randint
+num = randint(1,4)
 
-red = Color(0xff0000,1)
-line = LineStyle(3,red)
-rectangle = RectangleAsset(100,100,line,red)
+if num == 1:
+    red = Color(0xff0000,1)
+    green = Color(0x00ff00,1)
+    line = LineStyle(10,green)
+    rectangle = RectangleAsset(400,400,line,red)
 
-Sprite(rectangle)
-myApp = App()
-myApp.run()
+    Sprite(rectangle)
+    myApp = App()
+    myApp.run()
 
-blue = Color(0x0000ff,1)
-line = LineStyle(3,blue)
-rectangle = RectangleAsset(100,100,line,blue)
+elif num == 2: 
+    blue = Color(0x0000ff,1)
+    yellow = Color(0xffff00,1)
+    red = Color(0xff0000,1)
+    line = LineStyle(25,yellow)
+    line2 = LineStyle(10,red)
+    rectangle = RectangleAsset(200,100,line,blue)
 
-Sprite(rectangle)
-myApp = App()
-myApp.run()
+    Sprite(rectangle)
+    myApp = App()
+    myApp.run()
 
-green = Color(0x00ff00,1)
-line = LineStyle(3,green)
-rectangle = RectangleAsset(100,100,line,green)
+elif num == 3:
+    green = Color(0x00ff00,1)
+    blue = Color(0x0000ff,1)
+    line = LineStyle(3,blue)
+    rectangle = RectangleAsset(100,300,line,green)
 
-Sprite(rectangle)
-myApp = App()
-myApp.run()
+    Sprite(rectangle)
+    myApp = App()
+    myApp.run()
 
-yellow = Color(0xffff00,1)
-line = LineStyle(3,yellow)
-rectangle = RectangleAsset(100,100,line,yellow)
-
-Sprite(rectangle)
-myApp = App()
-myApp.run()
+else:
+    yellow = Color(0xffff00,1)
+    red = Color(0xff0000,1)
+    line = LineStyle(15,red)
+    rectangle = RectangleAsset(250,250,line,yellow)
+    
+    Sprite(rectangle)
+    myApp = App()
+    myApp.run()
