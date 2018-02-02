7489520a9cc998586721aa67c0e5998150c7cf80
CollinsLiam21 <35492820+CollinsLiam21@users.noreply.github.com>
2018-02-02 08:39:13 -0500

    Update slope.py

--- slope.py
+++ slope.py
@@ -1,1 +1,13 @@
+#Liam Collins
+#1/17/18
+#slope.py - Calculating slope and equation of line
 
+x1 = float(input('X1 = '))
+y1 = float(input('Y1 = '))
+x2 = float(input('X2 = '))
+y2 = float(input('Y2 = '))
+m = (y2-y1)/(x2-x1)
+print('slope =', m)
+
+b = y1-m*x1
+print('Equation of line: Y =', m,'x +', b)
