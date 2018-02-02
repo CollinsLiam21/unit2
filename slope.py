247a50f9da4ab2aac78b289d9343a3565d9cb0a1
CollinsLiam21 <35492820+CollinsLiam21@users.noreply.github.com>
2018-02-02 08:43:36 -0500

    Updated from Brython Server: 2/2/2018 8:43:35 AM

--- slope.py
+++ slope.py
@@ -6,8 +6,14 @@ x1 = float(input('X1 = '))
 y1 = float(input('Y1 = '))
 x2 = float(input('X2 = '))
 y2 = float(input('Y2 = '))
-m = (y2-y1)/(x2-x1)
-print('slope =', m)
 
-b = y1-m*x1
-print('Equation of line: Y =', m,'x +', b)
+if x1 != x2:
+    m = (y2-y1)/(x2-x1)
+    print('slope =', m)
+    b = y1-m*x1
+    print('Equation of line: Y =', m,'x +', b)
+
+elif x1 == x2:
+    print('slope is undefined, line is vertical')
+
+
