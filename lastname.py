99bbd7a30273c39f402755a2da52a8f825908236
CollinsLiam21 <35492820+CollinsLiam21@users.noreply.github.com>
2018-02-02 08:36:38 -0500

    Updated from Brython Server: 2/2/2018 8:36:37 AM

--- lastname.py
+++ lastname.py
@@ -1,1 +1,11 @@
+#Liam Collins
+#2/2/18
+#lastname.py
+
+name = input('Enter your last name: ')
+
+if name>'mark':
+    print('Second half of alphabet')
+else:
+    print('First half of alphabet')
 
