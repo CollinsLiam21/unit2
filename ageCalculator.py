06f3bc41444175bbad04854ba3d46a8605de9911
CollinsLiam21 <35492820+CollinsLiam21@users.noreply.github.com>
2018-02-05 08:00:01 -0500

    Update ageCalculator.py

--- ageCalculator.py
+++ ageCalculator.py
@@ -1,1 +1,11 @@
+#Liam Collins
+#2/2/18
+#ageCalculator.py
 
+from datetime import date
+
+year = input('Enter year you were born: ')
+month = input('Enter the month you were born in: ')
+day = input('Enter the day you were born on: ')
+
+date.today().year-year
